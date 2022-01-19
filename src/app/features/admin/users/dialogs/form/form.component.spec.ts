7e8ae0c25cb0b926e5fd0d0ff3800d97a93e7da4
Fabio Bedini aka whisher <io@ilwebdifabio.it>
2022-01-19 22:35:57 +0100

    testing

--- src/app/features/admin/users/dialogs/form/form.component.spec.ts
+++ src/app/features/admin/users/dialogs/form/form.component.spec.ts
@@ -1,11 +1,26 @@
+// Core
+import { Component, Input } from '@angular/core';
+
 // Testing
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 
 // Material
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+// Models
+import { UserResponseDto } from '@api/models';
+
 // Components
 import { AdminUsersDialogFormComponent } from './form.component';
+
+// Mocks
+@Component({
+  selector: 'admin-users-form',
+  template: ``
+})
+export class MockAdminUsersFormComponent {
+  @Input() user: UserResponseDto | undefined = undefined;
+}
 
 describe('AdminUsersDialogFormComponent', () => {
   let component: AdminUsersDialogFormComponent;
@@ -18,7 +33,10 @@ describe('AdminUsersDialogFormComponent', () => {
     waitForAsync(() => {
       TestBed.configureTestingModule({
         imports: [],
-        declarations: [AdminUsersDialogFormComponent],
+        declarations: [
+          MockAdminUsersFormComponent,
+          AdminUsersDialogFormComponent
+        ],
         providers: [
           {
             provide: MAT_DIALOG_DATA,
